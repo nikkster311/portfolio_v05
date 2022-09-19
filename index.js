e0409a1b26c85b7da2428f0b9e865366df3e7924
nrhermann <nhermann@ucdavis.edu>
2022-09-18 18:13:32 -0700

    added theme colors and functionality

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let lightTheme = true;
 
+function toggleTheme() {
+    console.log('running toggleTheme')
+    console.log('lighttheme - ' + lightTheme)
+    lightTheme = !lightTheme
+    console.log('changed -- lighttheme - ' + lightTheme)
+
+    document.body.setAttribute('data-theme', lightTheme === true ? 'light' : 'dark')
+}
