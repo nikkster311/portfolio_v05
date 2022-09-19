60babfdb69788009dc827a6eabbb8329adb5e751
nrhermann <nhermann@ucdavis.edu>
2022-09-18 18:23:02 -0700

    add top button scroll functionality

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function toggleTheme() {
 
     document.body.setAttribute('data-theme', lightTheme === true ? 'light' : 'dark')
 }
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        console.log('scrollFunction')
+        console.log(document.body.scrollTop)
+        // console.log(document.)
+        if (window.scrollY > 30 ) {
+            document.getElementById("topBtn").style.display = "block";
+        } else {
+            document.getElementById("topBtn").style.display = 'none';
+        }
+    }
+
+// When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
