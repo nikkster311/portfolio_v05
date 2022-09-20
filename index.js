fb82eb0962feea96949e83d2f4dca40c2e7a6463
nrhermann <nhermann@ucdavis.edu>
2022-09-19 22:18:53 -0700

    remove console.logs

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 let lightTheme = true;
 
 function toggleTheme() {
-    console.log('running toggleTheme')
-    console.log('lighttheme - ' + lightTheme)
     lightTheme = !lightTheme
-    console.log('changed -- lighttheme - ' + lightTheme)
-
     document.body.setAttribute('data-theme', lightTheme === true ? 'light' : 'dark')
+    document.getElementById('light-icon').style.display = (lightTheme === true ? 'none' : 'block')
+    document.getElementById('dark-icon').style.display = (lightTheme === true ? 'block' : 'none')
 }
-
 
 // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-        console.log('scrollFunction')
-        console.log(document.body.scrollTop)
-        // console.log(document.)
         if (window.scrollY > 30 ) {
             document.getElementById("topBtn").style.display = "block";
         } else {
