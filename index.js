63daf97a14a84afa39a6d25a8ceacc9915eec019
Nikki <nikkster311@gmail.com>
2022-12-30 08:45:39 -0800

    fonts and assets

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let lightTheme = true;
+let lightTheme = false;
 
 function toggleTheme() {
     lightTheme = !lightTheme
@@ -22,3 +22,17 @@ function toggleTheme() {
     function topFunction() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
+
+
+// var lastScrollTop = 0;
+//
+// // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+// element.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+//     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+//     if (st > lastScrollTop){
+//         // downscroll code
+//     } else {
+//         // upscroll code
+//     }
+//     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+// }, false);
