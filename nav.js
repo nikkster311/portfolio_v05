598a27e3b32b8ecf2550f69167c669577e653500
nikkster <nikkster311@gmail.com>
2023-03-02 18:01:02 -0800

    put repeated code in their own docs

---
+++ nav.js
@@ -0,0 +1,13 @@
+document.write('<div class="topnav flex">\
+<i class="btn fa-solid fa-house-chimney-window"></i>\
+<a href="#about">about</a>\
+<a href="#experience">experience</a>\
+<a class="media-show" href ="#personalProjects">projects</a>\
+<a class="media-hide" href ="#personalProjects">personal projects</a>\
+<a href="#volunteer">volunteer</a>\
+<button class="btn" onclick="toggleTheme()">\
+    <i id="light-icon" class="hide fa-solid fa-toggle-off"></i>\
+    <i id="dark-icon" style="display: none" class="show fa-solid fa-toggle-on"></i>\
+</button>\
+</div>\
+');
---
+++ topBtnContact.js
@@ -0,0 +1,9 @@
+document.write('<div class="rotate-email-container">\
+<p onclick="copyEmail()" onmouseout="emailHover()" id="rotate-email">\
+    <span class="tooltiptext" id="emailToolTip">\
+        Copy to clipboard\
+    </span>\
+    nikki@nikkster.tech</p>\
+</div>\
+<button class="btn" onclick="topFunction()" id="topBtn" title="Go to top">Top</button>\
+');
