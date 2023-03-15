1191894f3c2f881b3361e5f1466dfc6b2e51541d
nikkster <nikkster311@gmail.com>
2023-03-14 17:39:25 -0700

    build out footer, add to pages

---
+++ footer.js
@@ -0,0 +1,55 @@
+document.write('      <section id="footer" class="footer">\
+<div class="footer-wrapper">\
+  <h3>Reach Out</h3>\
+  <h4>\
+    Whether you are looking to hire a thoughtful developer, have a\
+    question, or just want to say hello, please reach out. I will do my\
+    best to get back to you in a timely maner!\
+  </h4>\
+  <div class="socials-container-footer">\
+    <a\
+      class="link-no-styling card-link"\
+      target="_blank"\
+      rel="noopener noreferrer"\
+      href="https://www.linkedin.com/in/nicole-hermann/"\
+    >\
+      <i class="fa-brands fa-linkedin"></i\
+    ></a>\
+    <a\
+      target="_blank"\
+      rel="noopener noreferrer"\
+      href="https://github.com/nikkster311"\
+      ><i class="fa-brands fa-github"></i\
+    ></a>\
+    <a\
+      class="resumeSocial footer-not-homepage"\
+      target="_blank"\
+      rel="noopener noreferrer"\
+      href="../Hermann_resume.pdf"\
+      download="Hermann_resume"\
+    >\
+      <button class="btn">Download my resume</button></a>\
+      <a\
+      class="resumeSocial footer-homepage"\
+      style="display: none;"\
+      target="_blank"\
+      rel="noopener noreferrer"\
+      href="./Hermann_resume.pdf"\
+      download="Hermann_resume"\
+    >\
+      <button class="btn">Download my resume</button></a>\
+    <a\
+      target="_blank"\
+      rel="noopener noreferrer"\
+      href="https://twitter.com/nikksterDotTech"\
+      ><i class="fa-brands fa-twitter"></i\
+    ></a>\
+    <a href="mailto:nikki@nikkster.tech"\
+      ><i class="fa-regular fa-envelope"></i\
+    ></a>\
+  </div>\
+  <p>Nikkster.tech was Built, Designed, and Created by Nikki Hermann</p>\
+</div>\
+</section>\
+');
+
