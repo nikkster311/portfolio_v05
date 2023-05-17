fa50924b02dc40233f4359694d02f89f6d79ca40
nikkster <nikkster311@gmail.com>
2023-05-16 19:06:28 -0700

    add swiper js, edit cards, edit sections

--- hiremenav.js
+++ hiremenav.js
@@ -1,14 +1,15 @@
-document.write('<div class="topnav flex" id="nav">\
-<a class="btn hire-me" href="./hireme.html">hire me</a>\
+document.write(
+  '<div class="topnav flex" id="nav">\
+<a class="btn hire-me" href="./hireme">hire me</a>\
 <a href="#about">about</a>\
 <a href="#experience">experience</a>\
 <a class="media-show" href ="#personalProjects">projects</a>\
 <a class="media-hide" href ="#personalProjects">personal projects</a>\
-<a href="./index.html#experience">portfolio</a>\
+<a href="./#experience">portfolio</a>\
 <button class="btn" onclick="toggleTheme()">\
     <i id="light-icon" style="display: block" class="fa-solid fa-toggle-off"></i>\
     <i id="dark-icon" style="display: none" class="show fa-solid fa-toggle-on"></i>\
 </button>\
 </div>\
-');
-
+'
+);
