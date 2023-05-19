f59261dc483ed00bd00fd08eb9693590266343b6
nikkster <nikkster311@gmail.com>
2023-05-18 23:02:28 -0700

    edit hire me nav

--- hiremenav.js
+++ hiremenav.js
@@ -1,11 +1,10 @@
 document.write(
   '<div class="topnav flex" id="nav">\
-<a class="btn hire-me" href="./hireme">hire me</a>\
-<a href="#about">about</a>\
-<a href="#experience">experience</a>\
-<a class="media-show" href ="#personalProjects">projects</a>\
-<a class="media-hide" href ="#personalProjects">personal projects</a>\
+<a href="#services">services</a>\
+<a href="#faq">faq</a>\
+<a href="#why">why me?</a>\
 <a href="./#experience">portfolio</a>\
+<a href="./contact">contact</a>\
 <button class="btn" onclick="toggleTheme()">\
     <i id="light-icon" style="display: block" class="fa-solid fa-toggle-off"></i>\
     <i id="dark-icon" style="display: none" class="show fa-solid fa-toggle-on"></i>\
