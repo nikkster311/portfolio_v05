83b4398ea64c52f88139b5cc30dc6e3e4e4ca65b
nikkster <nikkster311@gmail.com>
2023-06-11 17:31:23 -0700

    fix faq accordian

--- hireme.js
+++ hireme.js
@@ -60,20 +60,22 @@ window.onload = function () {
 };
 
 // FAQ
-var acc = document.getElementsByClassName("accordion");
-var i;
+window.onload = function () {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    var symbol = this.getElementsByClassName("accordion-symbol");
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      symbol[0].innerHTML = "+";
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      symbol[0].innerHTML = "-";
-    }
-  });
-}
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      var symbol = this.getElementsByClassName("accordion-symbol");
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        symbol[0].innerHTML = "+";
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        symbol[0].innerHTML = "-";
+      }
+    });
+  }
+};
