94aa74512399c0de1e6c322648d69670211528ba
nikkster <nikkster311@gmail.com>
2023-06-15 15:05:02 -0700

    redesign of footer for hireme and contact pages

---
+++ footerHireContact.js
@@ -0,0 +1,67 @@
+document.write(
+  '      <section id="footer-hireme-contact" class="footer">\
+  <div class="footer-wrapper">\
+    <img\
+        class="brand-logo"\
+        src="./imgs/logos/nikkstertech-high-resolution-logo-white-on-transparent-background.png"\
+    />\
+    <div class="footer-grid-wrapper">\
+      <div class="footer-grid">\
+        <div class="footer-links-a">\
+          <h4>\
+              <a class="footer-link" href="../#experience">Portfolio</a>\
+          </h4>\
+          <h4>\
+              <a class="footer-link" href="./hireme/#whoAmI">About</a>\
+          </h4>\
+          <h4>\
+              <a class="footer-link" href="./hireme/#faq">FAQ</a>\
+          </h4>\
+          <h4>\
+              <a class="footer-link" href="./contact">Contact</a>\
+          </h4>\
+        </div>\
+        <div class="socials-container-footer flex">\
+          <a\
+              class="link-no-styling"\
+              target="_blank"\
+              rel="noopener noreferrer"\
+              href="https://www.linkedin.com/in/nicole-hermann/"\
+          >\
+              <i class="fa-brands fa-linkedin"></i\
+          ></a>\
+          <a\
+              target="_blank"\
+              rel="noopener noreferrer"\
+              href="https://github.com/nikkster311"\
+              ><i class="fa-brands fa-github"></i\
+          ></a>\
+          <a\
+              target="_blank"\
+              rel="noopener noreferrer"\
+              href="https://instagram.com/nikkster.tech"\
+              ><i class="fa-brands fa-instagram"></i\
+          ></a>\
+          <a\
+              target="_blank"\
+              rel="noopener noreferrer"\
+              href="https://twitter.com/nikksterDotTech"\
+              ><i class="fa-brands fa-twitter"></i\
+          ></a>\
+          <a target="_blank" rel="noopener noreferrer" id="yelp-btn" href="https://www.yelp.com/biz/nikkster-tech-marysville">\
+          <i class="fa-brands fa-yelp"></i></a>\
+          <a href="mailto:nikki@nikkster.tech"\
+              ><i class="fa-regular fa-envelope"></i\
+          ></a>\
+        </div>\
+          <p class="footer-made-by">Nikkster.tech was Built, Designed, and Created by Nikki Hermann</p>\
+          <div class="footer-buttons">\
+          <p class="footer-btn-wrapper"><a class="btn footer-not-homepage" href="./contact" target="_blank">Schedule a call</a></p>\
+              <p class="footer-btn-wrapper"><a class="btn footer-not-homepage" href="https://ko-fi.com/nikkster" target="_blank"><i class="fa-solid fa-mug-hot"></i> Buy me a coffee</a></p>\
+          </div>\
+        </div>\
+      </div>\
+    </div>\
+  </section>\
+  '
+);
