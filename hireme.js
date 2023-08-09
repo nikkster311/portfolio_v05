2a83301bbdcdbafd004cbe9503e0828eeb2c3478
nikkster <nikkster311@gmail.com>
2023-08-08 20:11:05 -0700

    add pricing

--- hireme.js
+++ hireme.js
@@ -77,3 +77,43 @@ window.onload = function () {
     });
   }
 };
+
+// SLIDESHOW
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  let slideshowId = document.getElementById("slideshowSection");
+
+  slideshowId.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+  });
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-slideshow", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active-slideshow";
+}
