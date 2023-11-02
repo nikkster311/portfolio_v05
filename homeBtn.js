5af889ad0c656108d920cff19d9854c00f258bbc
nikkster <nikkster311@gmail.com>
2023-11-01 22:29:22 -0700

    change home button to back button, link hireme page to case studies

--- homeBtn.js
+++ homeBtn.js
@@ -1,3 +1,4 @@
 document.write(
-  '<a class="btn fixedBtn" id="homeBtn" href="https://nikkster.tech"><i class="btn fa-solid fa-house-chimney-window"></i></a>'
+  // '<a class="btn fixedBtn" id="homeBtn" href="https://nikkster.tech"><i class="btn fa-solid fa-house-chimney-window"></i></a>\
+  '<button class="btn fixedBtn" id="homeBtn"  onclick="history.back()"><i class="fa-solid fa-circle-chevron-left"></i></button>'
 );
