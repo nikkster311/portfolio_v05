087156fe9b07dae40afc7fab777fe1146e1ae2b4
nikkster <nikkster311@gmail.com>
2023-11-12 15:32:18 -0800

    contact page updates

--- contactnav.js
+++ contactnav.js
@@ -1,11 +1,15 @@
 document.write(
   '<div class="topnav flex" id="nav">\
-  <a class="btn hire-me" href="./hireme">hire me</a>\
-  <button class="btn" onclick="toggleTheme()">\
-  <i id="light-icon" style="display: block" class="fa-solid fa-toggle-off"></i>\
-  <i id="dark-icon" style="display: none" class="show fa-solid fa-toggle-on"></i>\
-  <i id="green-icon" style="display: none" class="show fa-solid fa-star"></i>\
+<a href="./hireme#services">services</a>\
+<a href="./hireme#testimonialsSection">reviews</a>\
+<a href="./hireme#myWork">my work</a>\
+<a href="./pricing">pricing</a>\
+<a href="./hireme#faq">faq</a>\
+<button class="btn" onclick="toggleTheme()">\
+    <i id="light-icon" style="display: block" class="fa-solid fa-toggle-off"></i>\
+    <i id="dark-icon" style="display: none" class="show fa-solid fa-toggle-on"></i>\
+    <i id="green-icon" style="display: none" class="show fa-solid fa-star"></i>\
 </button>\
-  </div>\
-  '
+</div>\
+'
 );
