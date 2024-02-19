475f89d1b6c8ca7cdc4a1ce77451be409261dde6
nikkster <nikkster311@gmail.com>
2024-02-19 12:47:13 -0800

    add "home", remove "faq"

--- contactnav.js
+++ contactnav.js
@@ -1,11 +1,11 @@
 document.write(
   '<div class="topnav flex" id="nav">\
+  <a href="./">home</a>\
 <a href="./#services">services</a>\
 <a href="./reviews">reviews</a>\
 <a href="./#myWork">my work</a>\
 <a href="./about">about</a>\
 <a href="https://form.jotform.com/233375947492065">get a quote</a>\
-<a href="./#faq">faq</a>\
 </div>\
 '
 );
