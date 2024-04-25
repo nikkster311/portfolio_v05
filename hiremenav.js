0c356c0fad6890991fdb4d783f0ac84f2f7e8a3b
nikkster <nikkster311@gmail.com>
2024-04-25 16:09:51 -0700

    edit home page menu

--- hiremenav.js
+++ hiremenav.js
@@ -5,8 +5,6 @@ document.write(
 <a href="./mywork">my work</a>\
 <a href="./about">about</a>\
 <a href="https://form.jotform.com/233375947492065">get a quote</a>\
-<a href="#faq">faq</a>\
-<a href="./contact">contact</a>\
 </div>\
 '
 );
